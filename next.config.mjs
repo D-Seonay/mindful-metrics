97c994fa0a68072d16ea1bf062eda0a2796033e4
Mathéo DELAUNAY <dseonay@gmail.com>
2026-04-14 15:28:33 +0200

    config: integrate serwist into next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
+import withSerwistInit from "@serwist/next";
+
+const withSerwist = withSerwistInit({
+  swSrc: "src/app/sw.ts",
+  swDest: "public/sw.js",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
-export default nextConfig;
+
+export default withSerwist(nextConfig);
